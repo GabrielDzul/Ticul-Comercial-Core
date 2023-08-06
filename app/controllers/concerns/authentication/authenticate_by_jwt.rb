8867aae8f336f74471ca98d4d5ecd615fd150243
gabrieldzul <gabrieldzul@gmail.com>
2023-08-05 18:16:25 -0600

    fixed jwt services

--- app/controllers/concerns/authentication/authenticate_by_jwt.rb
+++ app/controllers/concerns/authentication/authenticate_by_jwt.rb
@@ -2,10 +2,11 @@ module Authentication
   module AuthenticateByJWT
     def authenticate_request
       token, _options = token_and_options(request)
-      raise Error::Unauthorized if @current_user.blank?
+      raise Error::Unauthorized if token.blank?
 
       decoded = JWTServices::Decode.new.execute(token)
-      @current_user = UserRepository.instance.find_by!(id: decoded[:user_id])
+      user_id = decoded[:data][:user_id]
+      @current_user = UserRepository.instance.find_by!(id: user_id)
       raise Error::Unauthorized if @current_user.blank?
     end
   end
